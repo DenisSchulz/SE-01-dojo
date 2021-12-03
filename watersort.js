9ecd1f268f1e36a9e37154c3ad009ebd5ae3412a
DenisSchulz <denis.schulzdjd@gmail.com>
2021-12-03 15:11:30 +0100

    Add first part of move validity check (enoughSpace)

--- watersort.js
+++ watersort.js
@@ -20,6 +20,40 @@ function initBeakers() {
 
   console.log(beakers);
 }
+
+function enoughSpace(o, d) {
+  var volume = 0;
+  var space = 0;
+
+  for (i = 2; i >= 0; i--) {
+    if (beakers[o].slots[i] != 0) {
+      if (beakers[o].slots[i] === beakers[o].slots[i + 1] || volume === 0) {
+        volume += 1;
+      } else {
+        i = -1;
+      }
+    }
+  }
+
+  for (i = 2; i >= 0; i--) {
+    if (beakers[d].slots[i] === 0) {
+      space += 1;
+    } else {
+      i = -1;
+    }
+  }
+
+  if (volume <= space) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkMove(o, d) {
+  return enoughSpace(o, d);
+}
+
 /*
 const test = prompt("Test \n");
 
