618c3398dfa6841f832c930a2118472c68baddab
DenisSchulz <denis.schulzdjd@gmail.com>
2021-12-03 14:25:57 +0100

    Install prompt-sync nide module to make handling CL input easier

---
+++ watersort.js
@@ -0,0 +1,9 @@
+//require prompt-sync function from promt-sync node module to make working with CL input easier
+
+const prompt = require("prompt-sync")();
+
+/*
+const test = prompt("Test \n");
+
+console.log(test);
+*/
